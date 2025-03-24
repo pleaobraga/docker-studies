e073fa588bf84f2d84ba6858b2875814fcd20dda
root <root@pleao-note-gamer>
2025-03-24 16:08:43 -0300

    feat: make node install dependencies

--- node/docker-entrypoint.sh
+++ node/docker-entrypoint.sh
@@ -4,5 +4,11 @@ set -e  # Exit on error
 # Wait for the database to be ready
 dockerize -wait tcp://db:3306 -timeout 20s
 
+# Ensure dependencies are installed
+if [ ! -d "node_modules" ]; then
+    echo "Installing dependencies..."
+    npm ci --production
+fi
+
 # Execute the original command
 exec "$@"
