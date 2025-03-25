78ffdf5c35c23dde7d11a3fa9e47ca405d54f929
root <root@pleao-note-gamer>
2025-03-25 12:49:59 -0300

    feat: add db creation file

---
+++ init-scripts/create-db.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS nodedb;
+
+USE nodedb;
+
+CREATE TABLE IF NOT EXISTS people (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+INSERT INTO people (name) VALUES ('Pedro');
+INSERT INTO people (name) VALUES ('Wesley');
